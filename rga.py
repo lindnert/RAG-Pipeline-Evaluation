736cb049039c66ee859ba3932aad98f327f1ee18
lindnert <tim.lindner09@gmail.com>
2024-03-18 17:12:37 +0100

    Clean-Up

--- rga.py
+++ rga.py
@@ -1,3 +1,5 @@
+import os
+
 from langchain_community.vectorstores import FAISS
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
@@ -7,42 +9,28 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import TextLoader
 import langchain
 
-langchain.debug = True
-
-import PyPDF2
-import os
-
 from api_key import API_KEY
+
+langchain.debug = True
 
 question = "Why do people prefer RAG over previous methods?"
 
 # Data
 data = []
 number_of_docs = 0
-#number_of_pages = 0
 
 text_files = os.listdir('Texts')
 print(f'text_file_list: {text_files}')
 for text_file in text_files:
-    #with open(os.path.join('Texts', pdf_file), 'rb') as file:
     file_path = os.path.join('Texts', text_file)
     doc = TextLoader(file_path=file_path, encoding='utf-8').load()
-
-    """
-    # Read text from each page
-    for page_num in range(len(reader.pages)):
-        page = reader.pages[page_num]
-        data += page.extract_text()
-        number_of_pages += 1
-    print(f'{number_of_pages} pages loaded from {pdf_file}.')
-    """
 
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=50) # chunk-size by characters
     chunks = text_splitter.split_documents(doc)
     data.extend(chunks)
-
     number_of_docs += 1
-    print(f'{number_of_docs} docs loaded.')
+
+print(f'{number_of_docs} docs loaded.')
 
 # Processing
 """create a vector representation of the provided texts using OpenAI embedding mode
@@ -50,7 +38,7 @@ and stores them in the FAISS index for efficient retrieval."""
 vectorstore = FAISS.from_documents(
     data, embedding=OpenAIEmbeddings(openai_api_key=API_KEY)
 )
-print(f'Amount in vectorstore: {vectorstore.index.ntotal}')
+print(f'Chunks in vectorstore: {vectorstore.index.ntotal}')
 retriever = vectorstore.as_retriever()
 
 template = """Answer the question based only on the following context:
