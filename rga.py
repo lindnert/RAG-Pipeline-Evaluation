a9f844b1a61c9c62fbf526047f9497a66c71cddf
lindnert <tim.lindner09@gmail.com>
2024-03-27 15:47:21 +0100

    Limit amount of chunks retrieved by retriever to k

--- rga.py
+++ rga.py
@@ -13,11 +13,26 @@ from api_key import API_KEY
 
 langchain.debug = True
 
-question = "Why do people prefer RAG over previous methods?"
+question = "How can RAG pipelines be evaluated?"
 
 # Data
 data = []
 number_of_docs = 0
+
+
+def limit_prompt(prompt, max_size):
+    # Split the prompt into context and question
+    context, question = prompt['context'], prompt['question']
+
+    # Check if the context exceeds the maximum size
+    if len(context) > max_size:
+        # Truncate the context while preserving question
+        limited_context = context[:max_size]
+        # Combine limited context and question into a new prompt
+        limited_prompt = {'context': limited_context, 'question': question}
+        return limited_prompt
+    else:
+        return prompt
 
 text_files = os.listdir('Texts')
 print(f'text_file_list: {text_files}')
@@ -25,7 +40,7 @@ for text_file in text_files:
     file_path = os.path.join('Texts', text_file)
     doc = TextLoader(file_path=file_path, encoding='utf-8').load()
 
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=50) # chunk-size by characters
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200) # chunk-size by characters
     chunks = text_splitter.split_documents(doc)
     data.extend(chunks)
     number_of_docs += 1
@@ -39,7 +54,7 @@ vectorstore = FAISS.from_documents(
     data, embedding=OpenAIEmbeddings(openai_api_key=API_KEY)
 )
 print(f'Chunks in vectorstore: {vectorstore.index.ntotal}')
-retriever = vectorstore.as_retriever()
+retriever = vectorstore.as_retriever(k=4)
 
 template = """Answer the question based only on the following context:
 {context}
@@ -47,6 +62,22 @@ template = """Answer the question based only on the following context:
 Question: {question}
 """
 prompt = ChatPromptTemplate.from_template(template)
+
+# def condense_prompt(prompt: ChatPromptTemplate, llm, max_tokens: int) -> ChatPromptTemplate:
+#     messages = prompt.to_messages()
+#     print(messages)
+#     num_tokens = llm.get_num_tokens_from_messages(messages)
+#     print(f'number of tokens in prompt: {num_tokens}')
+#     ai_function_messages = messages[2:]
+#     print(f'ai_function messages: {ai_function_messages}')
+#     while num_tokens > max_tokens:
+#         ai_function_messages = ai_function_messages[2:]
+#         num_tokens = llm.get_num_tokens_from_messages(
+#             messages[:2] + ai_function_messages
+#         )
+#     messages = messages[:2] + ai_function_messages
+#     return ChatPromptTemplate(messages=messages)
+
 
 model = ChatOpenAI(openai_api_key=API_KEY, verbose=True)
 
@@ -61,3 +92,6 @@ answer = chain.invoke(question)
 
 print(f'Question: {question}')
 print(f'Answer: {answer}')
+
+
+
